7abbc77e0620d82e3187118b2076541572027743
Andrey Baranov <tread_lightly@list.ru>
2023-07-04 14:49:31 +1000

    7. Метод удаления заявки Tracker.delete [#211749 # [#211749]]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,4 +60,16 @@ public class Tracker {
         }
         return replaceItem;
     }
+
+    public boolean delete(int id) {
+        boolean isDelete = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+            isDelete = true;
+        }
+        return isDelete;
+    }
 }
