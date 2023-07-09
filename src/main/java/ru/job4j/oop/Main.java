8bbf47530b8dfbfa0108d20d94b32e1487094777
AVBaranov93 <avbaranov@internet.ru>
2023-07-09 17:46:13 +1000

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322 # [#181322]]

--- src/main/java/ru/job4j/oop/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.oop;
-
-public class Main {
-    public static void main(String[] args) {
-        Dog polkan = new Dog();
-        Dog sharik = new Dog();
-        Dog zhychka = new Dog();
-    }
-}
