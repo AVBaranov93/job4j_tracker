204e0619a955e08b5201e147c16d45ed9cfcbb39
Andrey Baranov <tread_lightly@list.ru>
2023-07-13 14:55:57 +1000

    2.1. Тест на ValidateInput [#311887 # [#311887]]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -175,4 +175,65 @@ class StartUITest {
                         + "=== Exit ===" + ln
         );
     }
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOut();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOut();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenMultipleValidInputs() {
+        Output out = new StubOut();
+        Input in = new StubInput(
+                new String[] {"0", "1", "2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(0);
+    }
+
+    @Test
+    public void whenNegativeInput() {
+        Output out = new StubOut();
+        Input in = new StubInput(
+                new String[] {"-2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-2);
+    }
+
+    @Test
+    void whenMenuItemIsNotNumber() {
+        Tracker tracker = new Tracker();
+        Output out = new StubOut();
+        String[] answers = new String[] {"zero", "0"};
+        Input input = new ValidateInput(out, new StubInput(answers));
+        UserAction[] actions = {new ExitAction(out)};
+        new StartUI(out).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu:" + ln
+                        + "0. Exit" + ln
+                        + "Please enter valid data" + ln
+                        + "=== Exit ===" + ln
+        );
+    }
 }
