5bb7419c2ecccda24b10cb4ece1caee4ffc8ae5e
AVBaranov93 <avbaranov@internet.ru>
2023-09-24 18:30:10 +1000

    4. Сортировка номера [#221284 #446986]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -1,14 +1,13 @@
 package ru.job4j.collection;
 
 import java.util.Comparator;
-import java.util.regex.Pattern;
 
 public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        Integer leftValue = Integer.parseInt(left.split(Pattern.quote("."))[0]);
-        Integer rightValue = Integer.parseInt(right.split(Pattern.quote("."))[0]);
+        Integer leftValue = Integer.parseInt(left.split("\\.")[0]);
+        Integer rightValue = Integer.parseInt(right.split("\\.")[0]);
         return Integer.compare(leftValue, rightValue);
     }
 }
