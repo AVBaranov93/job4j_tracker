79a84d83cefa67333c67c54d0e2be56d328208ec
AVBaranov93 <avbaranov@internet.ru>
2023-10-19 10:44:54 +1000

    10. Преобразование матрицы чисел в список чисел. FlatMap. [#110228]

--- src/test/java/ru/job4j/stream/MatrixToListTest.java
+++ src/test/java/ru/job4j/stream/MatrixToListTest.java
@@ -1,11 +1,9 @@
 package ru.job4j.stream;
 
-
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 class MatrixToListTest {
     @Test
